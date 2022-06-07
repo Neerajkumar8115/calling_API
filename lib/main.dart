be14c05c05aa138520228bf36a8b39ad42f02747
Neeraj kumar <nk04740gmail.com>
2022-06-07 10:27:32 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Myhomepage(),
+    );
+  }
+}
+
+class Myhomepage extends StatefulWidget {
+  const Myhomepage({Key? key}) : super(key: key);
+
+  @override
+  State<Myhomepage> createState() => _MyhomepageState();
+}
+
+class _MyhomepageState extends State<Myhomepage> {
+  getApidata() async {
+    var Url = Uri.parse('https://cat-fact.herokuapp.com/facts');
+    http.Response respons = await http.get(Url);
+    print(respons);
+    if (respons.statusCode == 200) {
+      final data = jsonDecode(respons.body);
+      final text = data[0]['text'];
+      print(text);
+    } else {
+      print('connection error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('call api'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+            onPressed: () {
+              getApidata();
+            },
+            child: const Text('data'),
+          ),
+          const Text(
+            'response',
+          ),
+        ]),
+      ),
+    );
+  }
+}
